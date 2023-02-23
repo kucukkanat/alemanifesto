a50223eb28d9bb2fa525c1e79ac5f6e52a37c4b3
kucukkanat <htolgasahin@gmail.com>
2023-02-23 01:17:43 +0100

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ */
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: '🍺🍻',
+  tagline: '3 Pillars Of Better Documentation',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://alemanifesto.com',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'kucukkanat', // Usually your GitHub org/user name.
+  projectName: 'alemanifesto', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/kucukkanat/alemanifesto/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/kucukkanat/alemanifesto/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: 'ALE Manifesto',
+        logo: {
+          alt: 'Ale Manifesto Logo',
+          src: 'img/pale-ale.png',
+        },
+        items: [
+          // {
+          //   type: 'doc',
+          //   docId: 'intro',
+          //   position: 'left',
+          //   label: 'Tutorial',
+          // },
+          // {to: 'blog', label: 'Blog', position: 'left'},
+          // // Please keep GitHub link to the right for consistency.
+          // {
+          //   href: 'https://github.com/facebook/docusaurus',
+          //   label: 'GitHub',
+          //   position: 'right',
+          // },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Hilmi Tolga SAHIN',
+            items: [
+              {
+                label: 'Github',
+                to: 'https://github.com/kucukkanat',
+              },
+              {
+                label: 'Linkedin',
+                to: 'https://www.linkedin.com/in/tolga-s-73664937/',
+              },
+            ],
+          },
+          // {
+          //   title: 'Community',
+          //   items: [
+          //     {
+          //       label: 'Stack Overflow',
+          //       href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+          //     },
+          //     {
+          //       label: 'Twitter',
+          //       href: 'https://twitter.com/docusaurus',
+          //     },
+          //     {
+          //       label: 'Discord',
+          //       href: 'https://discordapp.com/invite/docusaurus',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'More',
+          //   items: [
+          //     {
+          //       label: 'Blog',
+          //       to: 'blog',
+          //     },
+          //     {
+          //       label: 'GitHub',
+          //       href: 'https://github.com/facebook/docusaurus',
+          //     },
+          //   ],
+          // },
+          // {
+          //   title: 'Legal',
+          //   // Please do not remove the privacy and terms, it's a legal requirement.
+          //   items: [
+          //     {
+          //       label: 'Privacy',
+          //       href: 'https://opensource.fb.com/legal/privacy/',
+          //     },
+          //     {
+          //       label: 'Terms',
+          //       href: 'https://opensource.fb.com/legal/terms/',
+          //     },
+          //     {
+          //       label: 'Data Policy',
+          //       href: 'https://opensource.fb.com/legal/data-policy/',
+          //     },
+          //     {
+          //       label: 'Cookie Policy',
+          //       href: 'https://opensource.fb.com/legal/cookie-policy/',
+          //     },
+          //   ],
+          // },
+        ],
+        logo: {
+          alt: 'Meta Open Source Logo',
+          // This default includes a positive & negative version, allowing for
+          // appropriate use depending on your site's style.
+          src: '/img/pale-ale.png',
+          href: 'https://alemanifesto.com',
+        },
+        // Please do not remove the credits, help to publicize Docusaurus :)
+        copyright: `Copyright © ${new Date().getFullYear()} Hilmi Tolga SAHIN`,
+      },
+    }),
+};
+
+module.exports = config;
